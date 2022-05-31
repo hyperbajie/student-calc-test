1339bea5c5a18d6a87a15625f1b9985a7cc675b5
hyperbajie <maizhanwangle@163.com>
2022-05-31 21:34:46 +0800

    add not found pages

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { Home } from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import { TestPaper } from "./pages/TestPaper";
 import { InfiniteTest } from "./pages/InfiniteTest";
+import { NotFound } from "./pages/404";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="TestPaper" element={<TestPaper />}></Route>
-        <Route path="InfiniteTest" element={<InfiniteTest />}></Route>
+        <Route index element={<Home />}></Route>
+        <Route path="paper" element={<TestPaper />}></Route>
+        <Route path="test" element={<InfiniteTest />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </div>
   );
